6860e90d8b663d544c71bdeffc38d6b4f90e1949
Miguel Bittencourt <79655356+miguelbittencourt@users.noreply.github.com>
2023-02-12 12:48:33 -0300

    project upload

---
+++ Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Title { get; set; }
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [DisplayName("Última atualização")]
+        public DateTime LastUpdateAt { get; set; } = DateTime.Now;
+        [DisplayName("Usuário")]
+        public string User { get; set; }
+    }
+}
